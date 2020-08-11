f07b45a3a4e77757a5a0c195c3ab1d35e2587475
DaJuan Harris <dajuancancode@gmail.com>
2020-08-11 13:33:32 -0400

    Fixing deployment issues

--- src/index.js
+++ src/index.js
@@ -1,42 +1,33 @@
-require("dotenv").config()
+require("dotenv").config();
 
-const express = require("express")
-const cors = require("cors")
-const sgMail = require('@sendgrid/mail')
+const express = require("express");
+const cors = require("cors");
+const sgMail = require("@sendgrid/mail");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-sgMail.setApiKey(process.env.API_KEY)
+sgMail.setApiKey(process.env.API_KEY);
 
 app.post("/", async (req, res) => {
-  const body = req.body
+  const body = req.body;
   const msg = {
-    to: 'dajuancancode@gmail.com',
+    to: "dajuancancode@gmail.com",
     from: `${body.email}`,
     subject: `${body.firstName} ${body.lastName} sent you a message from http://youneedamc.com`,
-    content:  [
-      {type: "text/plain", value: `${body.message}`}
-    ]
-  }
+    content: [{ type: "text/plain", value: `${body.message}` }],
+  };
 
   try {
-    await sgMail.send(msg)
-    res.send("Sent")
-    console.log("Good to go")
-  } catch(error) {
-    console.error(error)
-
-    if(error.response) {
-      console.error(error.response.body)
-    }
-
-    res.send("Not Sent")
+    await sgMail.send(msg);
+    res.send("Sent");
+  } catch (error) {
+    res.status(400);
+    res.send("Not Sent");
   }
-  
-})
+});
 
-const port = 3001
-app.listen(port, () => console.log(`[app]: Listening on port ${port}`))
+const port = 3001;
+app.listen(port, () => console.log(`[app]: Listening on port ${port}`));
